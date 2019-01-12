21d35264ab658cd5437cf5e1fa79712e42a4268b
aceysx <aceysx@163.com>
2019-01-13 00:16:45 +0800

    [#3][Xin]: Fixed error.

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,14 +1,6 @@
-const {injectBabelPlugin} = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
+const {injectBabelPlugin} = require('react-app-rewired')
 
-module.exports = function override(config, env) {
+module.exports = function override (config, env) {
   config = injectBabelPlugin(['import', {libraryName: 'antd', style: true}], config)
-
-  config = rewireLess.withLoaderOptions({
-    modifyVars: {
-      '@primary-color': '#1973ba',
-      '@font-size-base': '14px'
-    }
-  })(config, env)
   return config
-};
+}
